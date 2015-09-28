5a6f56158bd14764d90b3c4d3d7c8b497a19253b
Michael Kirk <michael.winter.kirk@gmail.com>
2015-09-28 00:01:50 -0400

    Initial Commit

---
+++ Warehouse/Warehouse.h
@@ -0,0 +1,19 @@
+//
+//  Warehouse.h
+//  Warehouse
+//
+//  Created by Michael Kirk on 9/28/15.
+//  Copyright © 2015 Winterlane, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Warehouse.
+FOUNDATION_EXPORT double WarehouseVersionNumber;
+
+//! Project version string for Warehouse.
+FOUNDATION_EXPORT const unsigned char WarehouseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Warehouse/PublicHeader.h>
+
+
